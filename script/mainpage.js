442a9978939b01f4602eb084821067964c89a3d0
Shiitakay <matthewstepfner@gmail.com>
2024-10-06 14:59:26 +1100

    delete buttons delete projects, creating projects creates delete buttons for them

--- script/mainpage.js
+++ script/mainpage.js
@@ -1,4 +1,4 @@
-const debug = 0;
+const debug = 1;
 
 proj_btn = document.getElementById('new-project-btn');
 proj_btn.addEventListener('click', ()=>{show_hide_creation_form(true)});
@@ -8,6 +8,9 @@ cancel_btn.addEventListener('click', ()=>{show_hide_creation_form(false)});
 
 cancel_btn = document.getElementById('form-create');
 cancel_btn.addEventListener('click', create_project);
+
+delete_btns = Array.from(document.getElementsByClassName('delete-btn'));
+delete_btns.map((x)=>x.addEventListener('click', ()=>{delete_proj(x)}));
 
 function show_hide_creation_form(is_vis) {
   creation_form = document.getElementById('form-container');
@@ -30,6 +33,11 @@ function create_project_div(name_val) {
   new_project_div = document.createElement('div');
   new_project_div.setAttribute("class", "project-details");
 
+  proj_del = document.createElement('div');
+  proj_del.setAttribute("class", "delete-btn");
+  new_project_div.appendChild(proj_del);
+  proj_del.addEventListener('click', del_btn_closure(proj_del));
+
   proj_h1 = document.createElement('h1');
   if (name_val === "") {proj_h1.innerText = "New project";}
   else {proj_h1.innerText = name_val;}
@@ -41,6 +49,20 @@ function create_project_div(name_val) {
   new_project_div.appendChild(proj_p);
   if (debug) {
     console.log(new_project_div);
+    console.log(delete_btns);
   }
+  new_project_div.remove();
   return new_project_div;
+}
+
+// creates a closure for delete buttons
+// used for addEventListener on delete buttons
+function del_btn_closure(to_del) {
+  to_del;
+  return ()=>{delete_proj(to_del)};
+}
+
+// Given a button, removes its parent node
+function delete_proj(del_button) {
+  del_button.parentNode.remove();
 }
