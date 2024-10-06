f08513ae07b9366e92329d9e9f8fae990aa92c0e
Shiitakay <matthewstepfner@gmail.com>
2024-10-06 20:00:11 +1100

    moved common form scripts to own file TODO: edit this in mainpage

---
+++ script/form.js
@@ -0,0 +1,9 @@
+function show_hide_creation_form(is_vis) {
+  creation_form = document.getElementById('form-container');
+  console.log(creation_form);
+  new_val = "";
+  if (is_vis) {new_val = "visible"}
+  else {new_val = "hidden"}
+  creation_form.style.visibility = new_val;
+  
+}
